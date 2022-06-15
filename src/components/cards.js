5cdba394bb855710f166e8cd3c6bcddac857eade
alex tristram <tristram.alexander94@gmail.com>
2022-06-15 23:16:30 +0100

    added cards component

---
+++ src/components/cards.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Cards() {
+  return (
+    <div className="main-box">
+      <div className="small-box"></div>
+      <div className="small-box"></div>
+      <div className="small-box"></div>
+    </div>
+  );
+}
+
+export default Cards;
